0904cb2521018c8fa31de6213ad361428eeef1ef
Khushi Yadav <ky0307005@gmail.com>
2025-10-15 00:29:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Postorder/iterative-postorder.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};*/
+class Solution {
+  public:
+    vector<int> postOrder(Node* root) {
+        // code here
+        stack<Node*> s;
+        s.push(root);
+        vector<int> ans;
+        
+        while(!s.empty()){
+            Node* temp = s.top();
+            s.pop();
+            
+            ans.push_back(temp->data);
+            if(temp->left) s.push(temp->left);
+            if(temp->right) s.push(temp->right);
+            
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
